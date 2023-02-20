536dd45005b076755a0697ff32057ffe90e606c8
Tyler <tyler.mackay@barclays.com>
2023-02-20 15:02:23 +0000

    Task 1 completed

--- server.js
+++ server.js
@@ -6,7 +6,10 @@ const {sequelize} = require("./db");
 const port = 3000;
 
 //TODO: Create your GET Request Route Below: 
-
+app.get("/restaurants", async (req, res) => {
+    const restaurants = await Restaurant.findAll();
+    res.json(restaurants);
+  });
 
 app.listen(port, () => {
     sequelize.sync();
