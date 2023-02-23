321912c985f43bb8bd32d886f0a8fef230974a7a
Tyler <tyler.mackay@barclays.com>
2023-02-23 10:40:15 +0000

    Middleware added, Get,Delete,Put and Post Added

--- server.js
+++ server.js
@@ -5,12 +5,61 @@ const {sequelize} = require("./db");
 
 const port = 3000;
 
+app.use(express.json());
+
 //TODO: Create your GET Request Route Below: 
 app.get("/restaurants/:id", async (req, res) => {
   const {id} = req.params;
   const restaurants = await Restaurant.findByPk(id);
   res.json(restaurants);
 });
+
+// Create a new restaurant
+app.post("/restaurants", async (req, res) => {
+  const {name, address, phone} = req.body;
+  const restaurant = await Restaurant.create({name, address, phone});
+  res.json(restaurant);
+});
+
+// Update an existing restaurant
+app.put("/restaurants/:id", async (req, res) => {
+  const {id} = req.params;
+  const {name, address, phone} = req.body;
+  const [updated] = await Restaurant.update({name, address, phone}, {
+    where: {id}
+  });
+  if (updated) {
+    const updatedRestaurant = await Restaurant.findByPk(id);
+    res.json(updatedRestaurant);
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+// Delete a restaurant
+app.delete("/restaurants/:id", async (req, res) => {
+  const {id} = req.params;
+  const deleted = await Restaurant.destroy({
+    where: {id}
+  });
+  if (deleted) {
+    res.sendStatus(204);
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+// Get a restaurant by id
+app.get("/restaurants/:id", async (req, res) => {
+  const {id} = req.params;
+  const restaurant = await Restaurant.findByPk(id);
+  if (restaurant) {
+    res.json(restaurant);
+  } else {
+    res.sendStatus(404);
+  }
+});
+
 
 app.listen(port, () => {
     sequelize.sync();
